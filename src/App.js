e054a3847b81b2b61073946adabef26ffb4868bd
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-05-09 11:36:35 +0600

    added the meals summary and available meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main>
+        <Meals></Meals>
+      </main>
     </Fragment>
   );
 }
